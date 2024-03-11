0a394323d7b140860e90fd91d3dd85d9c561929d
Siboniso Nene <sibonisodev@gmail.com>
2024-03-11 14:48:27 +0000

    Change main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const country = document.getElementById("")
 
 async function getCountry(){
+    const country = document.getElementById("");
 
+    if (!country) {
+        alert("Please enter a country");
+        return;
+    }
 }
