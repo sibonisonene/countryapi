44bbf61937945a5ca7758d6f95f90d4fd12141e9
Siboniso Nene <sibonisodev@gmail.com>
2024-03-11 15:12:23 +0000

    Add endpoint

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const country = document.getElementById("")
 async function getCountry(){
     const country = document.getElementById("country-search");
 
+    const endpoint = new URL (`https://restcountries.com/v3.1/name/{country}?fullText=true`);
+    
     if (!country) {
         alert("Please enter a country");
         return;
